afa16fd05c40fb3fc9157a2c2ad3734da6fa4fb8
purushothaman-source <purushoth99jun@gmail.com>
2020-11-23 14:09:43 +0530

    ADDED :calculated wages for month

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -5,24 +5,28 @@ import java.util.Random;
 public class EmployeeWageComputation {
 
 	    public static void main(String[] args) {
-	    	Random random =new Random();
-			int randomnum =random.nextInt(3);
-			System.out.println("RandomNumber :"+randomnum);	
+	    	Random random =new Random();	
               int salary;
               int empHrs;
-				int EMP_RATE_PER_HR=20;
-				switch (randomnum) {
-				case 1:empHrs=4;
-					   break;          
-				case 2 :empHrs=8;
-				       break;
-				 default:
-				       empHrs=0;
-				}				
-					System.out.println("Employee hours: "+empHrs); 
-					salary=( empHrs * EMP_RATE_PER_HR ); 
-					System.out.println("Salary: "+salary);
-
+            	int	 EMP_RATE_PER_HR=20; 
+            		int NUM_WORKING_DAYS=20; 
+            		  int totalSalary=0 ;
+            		  for(int day=1  ; day <= NUM_WORKING_DAYS; day++  ) 
+            		  {
+            			  int randomnum =random.nextInt(3);
+            		          switch(randomnum) {  
+            		          case 1: empHrs=4 ;
+            		                   break; 
+            		          case 2: empHrs=8 ;
+            		                   break; 
+            		           default: empHrs=0; 
+            		          }    
+            		      salary=(  empHrs * EMP_RATE_PER_HR); 
+            		           System.out.println("Day: "+day+"--->>>>"+  "Salary:" +salary); 
+            		        totalSalary=(  totalSalary + salary );
+            		  }
+            		          
+            		  System.out.println( "Total Salary:" +totalSalary);
 	
 
 }
