3779dd0b9aa11ff22f91de6d77ed46216a446f97
purushothaman-source <purushoth99jun@gmail.com>
2020-11-23 13:07:58 +0530

    ADDED :welcome message

---
+++ EmployeeWageComputation.java
@@ -0,0 +1,13 @@
+package com.javaPrograms.employeeWageComputation;
+
+import java.util.Random;
+
+public class EmployeeWageComputation {
+
+	    public static void main(String[] args) {
+		System.out.println("welcome to employee wage computation proplem using java");
+
+
+	}
+
+}
