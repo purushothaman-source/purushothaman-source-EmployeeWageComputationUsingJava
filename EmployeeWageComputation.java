5967c9a7ea9cd166c523f4d83f140ce96aabe8a8
purushothaman-source <purushoth99jun@gmail.com>
2020-11-24 10:46:00 +0530

    ADDED :practise file

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -4,36 +4,24 @@ import java.util.Random;
 
 public class EmployeeWageComputation {
 
-	    public static void main(String[] args) {
-	    	Random random =new Random();	
-	    	int IS_PART_TIME=1; 
-	    	 int IS_FULL_TIME=2; 
-	    	int EMP_RATE_PER_HR=20;
-	    		int	MAX_HRS_IN_MONTH=100;
-	    		int	NUM_WORKING_DAYS=20 ;
-	    		int	totalEmpHrs=0 ;
-	    		int	totalWorkingDays=0;  
-	    		int randomnum;
-	    		int empHrs;
-	    		int totalSalary;
-	    			while( totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_WORKING_DAYS ) 
-	    			{
-	    			                totalWorkingDays++; 
-	    			                System.out.println("Total working day:" +totalWorkingDays);
-	    			                randomnum=random.nextInt(3); 
-	    			         switch(randomnum)
-	    			         {
-	    			         case 1: empHrs=4; 
-	    			                  break; 
-	    			         case 2: empHrs=8 ;
-	    			                   break; 
-	    			          default: empHrs=0; 
-	    			         }
-	    			          totalEmpHrs=(  totalEmpHrs + empHrs ); 
-	    			          System.out.println("Total Employee Hours: "+totalEmpHrs); 
-	    			}
-	    			totalSalary=(  totalEmpHrs * EMP_RATE_PER_HR);
-	    			System.out.println( "Total Salary: "+totalSalary);
+    public static void main(String[] args) {
+    	Random random =new Random();
+		int randomnum =random.nextInt(3);
+		System.out.println("RandomNumber :"+randomnum);	
+          int salary;
+          int empHrs;
+			int EMP_RATE_PER_HR=20;
+			switch (randomnum) {
+			case 1:empHrs=4;
+				   break;          
+			case 2 :empHrs=8;
+			       break;
+			 default:
+			       empHrs=0;
+			}				
+				System.out.println("Employee hours: "+empHrs); 
+				salary=( empHrs * EMP_RATE_PER_HR ); 
+				System.out.println("Salary: "+salary);
 
 
 
---
+++ Utility.java
@@ -0,0 +1,5 @@
+package com.javaPrograms.employeeWageComputation;
+
+public class Utility {
+
+}
