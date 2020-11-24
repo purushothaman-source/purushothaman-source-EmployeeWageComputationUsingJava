df20c4435b20b8e2952bfc35dfb02887b389f5f6
purushothaman-source <72643364+purushothaman-source@users.noreply.github.com>
2020-11-24 12:19:11 +0530

    Update EmployeeWageComputation.java

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -8,11 +8,11 @@ public class EmployeeWageComputation {
 	    	Random random =new Random();	
               int salary;
               int empHrs;
-            	int	 EMP_RATE_PER_HR=20; 
-            		int NUM_WORKING_DAYS=20; 
-            		  int totalSalary=0 ;
-            		  for(int day=1  ; day <= NUM_WORKING_DAYS; day++  ) 
-            		  {
+              int EMP_RATE_PER_HR=20; 
+              int NUM_WORKING_DAYS=20; 
+              int totalSalary=0 ;
+              for(int day=1  ; day <= NUM_WORKING_DAYS; day++  ) 
+              {
             			  int randomnum =random.nextInt(3);
             		          switch(randomnum) {  
             		          case 1: empHrs=4 ;
@@ -20,14 +20,12 @@ public class EmployeeWageComputation {
             		          case 2: empHrs=8 ;
             		                   break; 
             		           default: empHrs=0; 
-            		          }    
+            		                            }    
             		      salary=(  empHrs * EMP_RATE_PER_HR); 
             		           System.out.println("Day: "+day+"--->>>>"+  "Salary:" +salary); 
             		        totalSalary=(  totalSalary + salary );
-            		  }
+             }
             		          
-            		  System.out.println( "Total Salary:" +totalSalary);
-	
-
+           System.out.println( "Total Salary:" +totalSalary);
 }
 }
